1129f785df587f5f39961998a9a614b5eb03ca0d
hana-7 <hananurkarimah26@gmail.com>
2021-06-06 17:12:57 +0700

    Initial commit

---
+++ app/src/main/java/com/example/receivingsmsmessages/SMSReceiver.java
@@ -0,0 +1,37 @@
+package com.example.receivingsmsmessages;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.os.Build;
+import android.os.Bundle;
+import android.provider.Telephony;
+import android.provider.Telephony.Sms.Intents;
+import android.telephony.SmsMessage;
+import android.util.Log;
+import android.widget.Toast;
+
+import androidx.annotation.RequiresApi;
+
+public class SMSReceiver extends BroadcastReceiver {
+    @RequiresApi(api = Build.VERSION_CODES.KITKAT)
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        //---get the SMS message passed in---
+        Bundle bundle = intent.getExtras();
+        SmsMessage[] msgs = null;
+        String str = "SMS from ";
+        if (bundle != null) {
+            //---retrieve the SMS message received---
+            msgs = Telephony.Sms.Intents.getMessagesFromIntent(intent);
+
+            for (int i = 0; i < msgs.length; i++) {
+                str += msgs[i].getMessageBody().toString();
+            }
+
+            //---display the new SMS message---
+            Toast.makeText(context, str, Toast.LENGTH_SHORT).show();
+            Log.d("SMSReceiver", str);
+        }
+    }
+}
